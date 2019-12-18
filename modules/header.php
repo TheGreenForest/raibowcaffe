d970abed5209e2af3e1c939c455d1553a99ce8f7
TheGreenForest <tgfagibalov@gmail.com>
2019-12-18 13:55:05 +0300

    Прост)

--- modules/header.php
+++ modules/header.php
@@ -1,32 +1,30 @@
 <div class="header">
-        <div class="up-menu">
-            <div class="container flex s-between ">
-                <div class="logo">
-                    <div class="logo-image"><img src="image/logo/rainbow-logo.svg" alt="Логотип" style="width: 100%;"></div>
-                    <div class="logo-text">
-                        <div class="logo-text-main">Кафе Радуга</div>
-                        <div class="logo-text-discription">"Хорошее блюдо - это блюдо из свежих продуктов. <br>Каждое блюдо требует свою порцию любви".</div>
-                    </div>
-                </div>
-                <div class="delivery"><p>При заказе от <span style="font-size:25px">500</span> рублей <br> доставка бесплатно</p> 
-                </div>
-                <div class="nav">
-                    <ul type="none" class="nav-menu">
-                        <li><a href="/index.php">Главная</a></li>
-                        <li><a href="/about.php">О нас</a></li>
-                        <li><a href="/contact.php">Контакты</a></li>
-                        <div class="menu-item">
-                            <li><a href="/menu.php">Меню</a></li>
-                            
-                        </div>
-                        <li><a href="/cart.php">Корзина</a></li>
-                    </ul>
-                </div>
-                <div class="header-phone">
-                    <p>+7 (495) 429-88-88</p>
-                    <p>+7 (966) 099-48-48</p>
+    <div class="up-menu">
+        <div class="container flex s-between ">
+            <div class="logo">
+                <div class="logo-image"><img src="image/logo/rainbow-logo.svg" alt="Логотип" style="width: 100%;"></div>
+                <div class="logo-text">
+                    <div class="logo-text-main">Кафе Радуга</div>
+                    <div class="logo-text-discription">"Хорошее блюдо - это блюдо из свежих продуктов. <br>Каждое блюдо требует свою порцию любви".</div>
                 </div>
             </div>
+            <div class="delivery">
+                <p>При заказе от <span style="font-size:25px">500</span> рублей <br> доставка бесплатно</p>
+            </div>
+            <div class="nav">
+                <ul type="none" class="nav-menu">
+                    <li><a href="/index.php">Главная</a></li>
+                    <li><a href="/about.php">О нас</a></li>
+                    <li><a href="/contact.php">Контакты</a></li>
+                    <li><a href="/menu.php">Меню</a></li>
+                    <li><a href="/cart.php">Корзина</a></li>
+                </ul>
+            </div>
+            <div class="header-phone">
+                <p>+7 (495) 429-88-88</p>
+                <p>+7 (966) 099-48-48</p>
+            </div>
         </div>
-        
     </div>
+
+</div>
